d541d03ac04eb4a883b9dc5c3048799af71db5ef
Paolo Sasso <sasso@mac.com>
2023-12-04 19:29:42 +0000

    day 4 - refactoring

--- src/day04/AoC04.kt
+++ src/day04/AoC04.kt
@@ -6,48 +6,37 @@ fun main() {
 	TheDay.run()
 }
 
-private object TheDay : DayList<Int, Int>(13, 30) {
+private object TheDay : DayList<Int, Int>(13, 30, 25231, 9721255) {
 
-	override fun part1(lines: List<String>): Int = lines.asSequence()
-		.map { it.substringAfter(": ") }
-		.map {
-			val (win, have) = it.split(" | ")
-			val w = win.chunked(3).map { it.trim().toInt() }.toSet()
-			val h = have.chunked(3).map { it.trim().toInt() }.toSet()
-			w to h
+	private fun parseLine(it: String): Pair<Set<String>, Set<String>> {
+		val (_, win, have) = it.split(":", " |")
+		val w = win.chunked(3).map { it }.toSet()
+		val h = have.chunked(3).map { it }.toSet()
+		return w to h
+	}
+
+	override fun part1(lines: List<String>): Int = lines
+		.map(::parseLine)
+		.sumOf {
+			val count = it.first.intersect(it.second).size
+			if (count == 0) 0 else 1 shl (count - 1)
 		}
-		.map {
-			it.first.intersect(it.second).size
-		}
-		.map {
-			when (it) {
-				0    -> 0
-				else -> 1 shl (it - 1)
-			}
-		}
-		.sum()
 
 	data class CardInfo(val win: Int, var count: Int = 1)
 
-	override fun part2(lines: List<String>): Int {
-		val list = lines
-			.map { it.substringAfter(": ") }
-			.map {
-				val (win, have) = it.split(" | ")
-				val w = win.chunked(3).map { it.trim().toInt() }.toSet()
-				val h = have.chunked(3).map { it.trim().toInt() }.toSet()
-				w to h
-			}
-			.map {
-				CardInfo(it.first.intersect(it.second).size)
-			}
-
-		list.forEachIndexed { index, card ->
-			(index + 1 .. index + card.win).forEach {
-				list[it].count += card.count
-			}
+	override fun part2(lines: List<String>): Int = lines
+		.map(::parseLine)
+		.map {
+			CardInfo(it.first.intersect(it.second).size)
 		}
+		.let { list ->
+			list.forEachIndexed { index, card ->
+				(index + 1..index + card.win).forEach {
+					list[it].count += card.count
+				}
+			}
+			list
+		}
+		.sumOf { it.count }
 
-		return list.sumOf { it.count }
-	}
 }
