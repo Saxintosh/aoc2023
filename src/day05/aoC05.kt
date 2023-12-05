002838b96eb618e1b0a02d36afed46051bac54ca
Paolo Sasso <sasso@mac.com>
2023-12-05 23:56:49 +0000

    day 5 - refactoring

--- src/day05/aoC05.kt
+++ src/day05/aoC05.kt
@@ -1,6 +1,8 @@
 package day05
 
 import DayList
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
 	TheDay.run()
@@ -9,7 +11,6 @@ fun main() {
 private object TheDay : DayList<Long, Long>(35L, 46L, 227653707L, 78775051L) {
 	class MyRange(val dst: Long, val src: Long, val len: Long) {
 		val rSrc = (src until src + len)
-		val rDst = (dst until dst + len)
 		val delta get() = dst - src
 		operator fun contains(x: Long) = x in rSrc
 		override fun toString() = "$src->$dst($len)"
@@ -24,7 +25,7 @@ private object TheDay : DayList<Long, Long>(35L, 46L, 227653707L, 78775051L) {
 		.map(String::toLong)
 
 
-	fun collectBlocks(lines: List<String>) = lines
+	fun collectBlocks(lines: List<String>): List<List<MyRange>> = lines
 		.drop(3)
 		.fold(mutableListOf(mutableListOf<MyRange>())) { acc, line ->
 			val parts = line.split(" ")
@@ -36,7 +37,7 @@ private object TheDay : DayList<Long, Long>(35L, 46L, 227653707L, 78775051L) {
 				}
 			}
 			acc
-		}
+		}.onEach { rList -> rList.sortBy { it.src } }
 
 	fun collectSeedsRange(lines: List<String>) = lines[0].substringAfter(": ")
 		.split(" ")
@@ -53,18 +54,51 @@ private object TheDay : DayList<Long, Long>(35L, 46L, 227653707L, 78775051L) {
 		}
 	}
 
+	// 5m 20s
+//	override fun part2(lines: List<String>): Long {
+//		val seeds = collectSeedsRange(lines)
+//		val blocks = collectBlocks(lines)
+//		return seeds.parallelStream()
+//			.map { lRange ->
+//				lRange.asSequence()
+//					.map {
+//						blocks.fold(it) { i, list -> list.cvt(i) }
+//					}
+//					.min()
+//			}
+//			.toList()
+//			.min()
+//	}
+
+	// 1ms
 	override fun part2(lines: List<String>): Long {
-		val seeds = collectSeedsRange(lines)
+		var ranges = collectSeedsRange(lines)
 		val blocks = collectBlocks(lines)
-		return seeds.parallelStream()
-			.map { lRange ->
-				lRange.asSequence()
-					.map {
-						blocks.fold(it) { i, list -> list.cvt(i) }
+		blocks.forEach { rList ->
+			val newRanges = mutableListOf<LongRange>()
+			ranges.forEach { range ->
+				var min = range.first
+				val max = range.last
+				for (r in rList) {
+					if (r.rSrc.last < min)
+						continue
+					if (r.rSrc.first > max)
+						break
+					if (min < r.rSrc.first) {
+						newRanges.add(min until r.rSrc.first)
+						min = r.rSrc.first
 					}
-					.min()
+					val bottom = max(min, r.rSrc.first)
+					val top = min(max, r.rSrc.last)
+					newRanges.add((bottom + r.delta) .. (top + r.delta))
+					min = top + 1
+					continue
+				}
+				if (min < max)
+					newRanges.add(min .. max)
 			}
-			.toList()
-			.min()
+			ranges = newRanges
+		}
+		return ranges.minOf { it.first }
 	}
 }
