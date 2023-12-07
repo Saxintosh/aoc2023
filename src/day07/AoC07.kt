1ed735c4d4e2348b9782cbc6315bd5b4b8f5ee80
Paolo Sasso <sasso@mac.com>
2023-12-07 23:36:09 +0000

    day 7 - refactoring

--- src/day07/AoC07.kt
+++ src/day07/AoC07.kt
@@ -8,77 +8,71 @@ fun main() {
 
 private object TheDay : DayList<Long, Long>(6440L, 5905L, 250347426L, 251224870L) {
 
-	private class Hand(hand: String, val bid: Long, val withJ: Boolean = false) : Comparable<Hand> {
+	private class Hand(val hand: String, val bid: Long) {
 		companion object {
 			var orderMap: Map<Char, Char> = emptyMap()
+
 			fun setOrder(str: String) {
 				val cardFigure = str.toList()
 				val cardOrder = cardFigure.indices.toList().map { 'Z' - it }
 				orderMap = cardFigure.zip(cardOrder).toMap()
 			}
+
+			fun parse(line: String) = line
+				.split(' ')
+				.let { Hand(it[0], it[1].toLong()) }
 		}
 
 		val orderedHand = hand.map { orderMap[it] }.joinToString("")
-		val type = hand.groupingBy { it }.eachCount().toList().sortedByDescending { it.second }
-			.let { pList ->
-				if (!withJ)
-					pList
-				else {
-					val theJ = pList.find { it.first == 'J' } ?: return@let pList
-					val noJList = (pList - theJ).ifEmpty { listOf('A' to 0) }
-					val head = noJList.first()
-					val tail = noJList.drop(1)
-					val newHead = head.first to (head.second + theJ.second)
-					listOf(newHead) + tail
-				}
-			}
-			.let {
-				val first = it.first().second
-				val second = it.getOrNull(1)?.second
-				val r = when (first) {
-					5    -> 7
-					4    -> 6
-					3    -> if (second == 2) 5 else 4
-					2    -> if (second == 2) 3 else 2
-					else -> 1
-				}
-				r
-			}
 
-		override fun compareTo(other: Hand): Int {
-			val order = type - other.type
-			if (order == 0)
-				return orderedHand.compareTo(other.orderedHand)
-			return order
+		fun rankList(cards: String) = cards
+			.groupingBy { it }
+			.eachCount()
+			.values
+			.sortedDescending()
+
+		fun rank(rl: List<Int>): Int {
+			val first = rl.first()
+			val second = rl.getOrNull(1)
+			return when {
+				first == 5                -> 10
+				first == 4                -> 9
+				first == 3 && second == 2 -> 8
+				first == 3                -> 7
+				first == 2 && second == 2 -> 6
+				first == 2                -> 5
+				else                      -> 4
+			}
+		}
+
+		fun type() = rank(rankList(hand))
+
+		fun typeWithJ(): Int {
+			val jCount = hand.count { it == 'J' }
+			val rl = rankList(hand.replace("J", ""))
+			val nrl = listOf((rl.firstOrNull() ?: 0) + jCount) + rl.drop(1)
+			return rank(nrl)
 		}
 	}
 
-	override fun part1(lines: List<String>) = lines
-		.also {
-			Hand.setOrder("AKQJT98765432")
-		}
-		.map {
-			val s = it.split(' ')
-			Hand(s[0], s[1].toLong())
-		}
-		.sorted()
-		.mapIndexed { i, h ->
-			(i + 1L) * h.bid
-		}
-		.sum()
+	override fun part1(lines: List<String>): Long {
+		Hand.setOrder("AKQJT98765432")
+
+		return lines
+			.map { Hand.parse(it) }
+			.sortedWith(compareBy({ it.type() }, { it.orderedHand }))
+			.mapIndexed { i, h -> (i + 1L) * h.bid }
+			.sum()
+	}
 
 
-	override fun part2(lines: List<String>) = lines
-		.also {
-			Hand.setOrder("AKQT98765432J")
-		}
-		.map {
-			val s = it.split(' ')
-			Hand(s[0], s[1].toLong(), true)
-		}
-		.sorted()
-		.mapIndexed { i, h ->
-			(i + 1L) * h.bid
-		}
-		.sum()
+	override fun part2(lines: List<String>): Long {
+		Hand.setOrder("AKQT98765432J")
+
+		return lines
+			.map { Hand.parse(it) }
+			.sortedWith(compareBy({ it.typeWithJ() }, { it.orderedHand }))
+			.mapIndexed { i, h -> (i + 1L) * h.bid }
+			.sum()
+	}
 }
