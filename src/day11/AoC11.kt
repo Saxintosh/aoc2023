e669e30b206a23311ea0f25763a4191770b92d98
Paolo Sasso <sasso@mac.com>
2023-12-11 08:59:34 +0000

    day 11 - refactoring

--- src/day11/AoC11.kt
+++ src/day11/AoC11.kt
@@ -9,39 +9,26 @@ fun main() {
 	TheDay.run()
 }
 
-private object TheDay : DayList<Int, Long>(374, 82000210L) {
+private object TheDay : DayList<Long, Long>(374L, 82000210L, 10228230L, 447073334102L) {
 	var xExp = emptyList<Int>()
 	var yExp = emptyList<Int>()
 
-	fun shortestPaths(pair: Pair<Point, Point>): Int {
+	var expansion = 0L
+
+	fun shortestPaths(pair: Pair<Point, Point>): Long {
 		val (p1, p2) = pair
 		val rx = myRange(p1.x, p2.x)
 		val ry = myRange(p1.y, p2.y)
 
-		var res = rx.last() - rx.first()
-		res += (ry.last() - ry.first())
-		res += (rx.count { it in xExp })
-		res += (ry.count { it in yExp })
+		var res = (rx.last() - rx.first()).toLong()
+		res += (ry.last() - ry.first()).toLong()
+		res += (rx.count { it in xExp }).toLong() * (expansion - 1)
+		res += (ry.count { it in yExp }).toLong() * (expansion - 1)
 
 		return res
 	}
 
-	fun shortestPaths2(pair: Pair<Point, Point>): Long {
-		val (p1, p2) = pair
-		val rx = myRange(p1.x, p2.x)
-		val ry = myRange(p1.y, p2.y)
-
-		var res = rx.last() - rx.first()
-		res += (ry.last() - ry.first())
-
-		var res2 = res.toLong()
-		res2 += (rx.count { it in xExp }).toLong() * (1000000L - 1)
-		res2 += (ry.count { it in yExp }).toLong() * (1000000L - 1)
-
-		return res2
-	}
-
-	override fun part1(lines: List<String>): Int {
+	private fun pairs(lines: List<String>): List<Pair<Point, Point>> {
 		val grid = ChGrid(lines)
 		yExp = grid.yRange.filter { y -> lines[y].all { it == '.' } }
 		xExp = grid.xRange.filter { x ->
@@ -58,28 +45,16 @@ private object TheDay : DayList<Int, Long>(374, 82000210L) {
 					}
 			}
 		}
+		return pairs
+	}
 
-		return pairs.sumOf { shortestPaths(it) }
+	override fun part1(lines: List<String>): Long {
+		expansion = 2
+		return pairs(lines).sumOf { shortestPaths(it) }
 	}
 
 	override fun part2(lines: List<String>): Long {
-		val grid = ChGrid(lines)
-		yExp = grid.yRange.filter { y -> lines[y].all { it == '.' } }
-		xExp = grid.xRange.filter { x ->
-			grid.yRange.all { y -> grid[x, y] == '.' }
-		}
-
-		val galaxies = grid.asPointsSequence().filter { grid[it] == '#' }.toList()
-		val pairs = galaxies.let { list ->
-			buildList {
-				val size = list.size
-				for (a in 0 until size)
-					for (b in (a + 1) until size) {
-						add(list[a] to list[b])
-					}
-			}
-		}
-
-		return pairs.sumOf { shortestPaths2(it) }
+		expansion = 1_000_000
+		return pairs(lines).sumOf { shortestPaths(it) }
 	}
 }
